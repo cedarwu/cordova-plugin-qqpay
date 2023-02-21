c6edb89242b557f3b7c9d056c372ccbb0faae0cb
cedarwu <cedarwu@tencent.com>
2023-02-21 18:03:19 +0800

    add android lib and plugin code

---
+++ src/android/callback-activity.java
@@ -0,0 +1,96 @@
+package __PACKAGE_NAME__;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tencent.mobileqq.openpay.api.IOpenApi;
+import com.tencent.mobileqq.openpay.api.IOpenApiListener;
+import com.tencent.mobileqq.openpay.api.OpenApiFactory;
+import com.tencent.mobileqq.openpay.data.base.BaseResponse;
+import com.tencent.mobileqq.openpay.data.pay.PayResponse;
+
+import cedar.cordova.qqpay.QQPay;
+
+public class CallbackActivity extends Activity implements IOpenApiListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        QQPay.getIOpenApi().handleIntent(getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+
+        QQPay.getIOpenApi().handleIntent(intent, this);
+    }
+
+    @Override
+    public void onOpenResponse(BaseResponse response) {
+        Log.d(QQPay.TAG, "Callback:" + response.toString());
+        CallbackContext ctx = QQPay.getCurrentCallbackContext();
+
+        if (response == null) {
+            Log.d(QQPay.TAG, "response is null");
+            ctx.error(QQPay.ERROR_RESPONSE_NULL);
+            finish();
+            return;
+        }
+
+        if (!(response instanceof PayResponse)) {
+            Log.d(QQPay.TAG, "response is not PayResponse");
+            ctx.error(QQPay.ERROR_RESPONSE_TYPE_INVALID);
+            finish();
+            return;
+        }
+
+        PayResponse payResponse = (PayResponse) response;
+
+        message = "apiName:" + payResponse.apiName + " serialnumber:"
+                + payResponse.serialNumber + " isSucess:"
+                + payResponse.isSuccess() + " retCode:"
+                + payResponse.retCode + " retMsg:" + payResponse.retMsg;
+
+        JSONObject resp = new JSONObject();
+        try {
+            resp.put("apiName", payResponse.apiName);
+            resp.put("serialNumber", payResponse.serialNumber);
+            resp.put("isSuccess", payResponse.isSuccess());
+            resp.put("isPayByWeChat", payResponse.isPayByWeChat());
+            resp.put("retCode", payResponse.retCode);
+            resp.put("retMsg", payResponse.retMsg);
+        } catch (JSONException e) {
+            System.out.println(e);
+        }
+
+        if (payResponse.isSuccess()) {
+            if (!payResponse.isPayByWeChat()) {
+                message += " transactionId:"
+                        + payResponse.transactionId + " payTime:"
+                        + payResponse.payTime + " callbackUrl:"
+                        + payResponse.callbackUrl + " totalFee:"
+                        + payResponse.totalFee + " spData:"
+                        + payResponse.spData;
+
+                try {
+                    resp.put("transactionId", payResponse.transactionId);
+                    resp.put("payTime", payResponse.payTime);
+                    resp.put("callbackUrl", payResponse.callbackUrl);
+                    resp.put("totalFee", payResponse.totalFee);
+                    resp.put("spData", payResponse.spData);
+                } catch (JSONException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        ctx.success(resp); // 回调
+        finish();
+    }
+}
---
+++ src/android/qqpay.java
@@ -0,0 +1,204 @@
+package cedar.cordova.qqpay;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPreferences;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tencent.mobileqq.openpay.api.IOpenApi;
+import com.tencent.mobileqq.openpay.api.IOpenApiListener;
+import com.tencent.mobileqq.openpay.api.OpenApiFactory;
+import com.tencent.mobileqq.openpay.data.base.BaseResponse;
+import com.tencent.mobileqq.openpay.data.pay.PayResponse;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class QQPay extends CordovaPlugin {
+
+    public static final String TAG = "Cordova.Plugin.QQPay";
+
+    public static final String APPID_PROPERTY_KEY = "qqappid";
+
+    public static final String ERROR_QQ_NOT_INSTALLED = "未安装QQ";
+    public static final String ERROR_INVALID_PARAMETERS = "参数格式错误";
+    public static final String ERROR_SEND_REQUEST_FAILED = "发送请求失败";
+    public static final String ERROR_RESPONSE_NULL = "回调结果为空";
+    public static final String ERROR_RESPONSE_TYPE_INVALID = "回调类型错误";
+
+    protected static IOpenApi openApi;
+    protected static CallbackContext currentCallbackContext;
+
+    protected static String appId;
+    protected static String callbackScheme;
+
+    int paySerial = 1;
+
+    @Override
+    protected void pluginInitialize() {
+
+        super.pluginInitialize();
+        initQQPay();
+    }
+
+    protected void initQQPay() {
+        if (openApi == null) {
+            String appId = getAppId();
+            openApi = OpenApiFactory.getInstance(this.cordova.getActivity(), appId);
+        }
+    }
+
+    protected String getAppId() {
+        if (appId == null) {
+            appId = preferences.getString(APPID_PROPERTY_KEY, "");
+            callbackScheme = "qwallet" + appId;
+        }
+        return appId;
+    }
+
+    public static CallbackContext getCurrentCallbackContext() {
+        return currentCallbackContext;
+    }
+
+    public static IOpenApi getIOpenApi() {
+        return openApi;
+    }
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) {
+        Log.d(TAG, String.format("%s is called. Callback ID: %s.", action, callbackContext.getCallbackId()));
+
+        if (action.equals("isMobileQQInstalled")) {
+            return isMobileQQInstalled(callbackContext);
+        } else if (action.equals("isMobileQQSupportApi")) {
+            return isMobileQQSupportApi(callbackContext);
+        } else if (action.equals("isMobileQQSupportPay")) {
+            return isMobileQQSupportApi(callbackContext);
+        } else if (action.equals("mqqPay")) {
+            return mqqPay(args, callbackContext);
+        }
+
+        return false;
+    }
+
+    protected boolean isMobileQQInstalled(CallbackContext callbackContext) {
+        boolean isInstalled = openApi.isMobileQQInstalled();
+
+        if (!isInstalled) {
+            callbackContext.success(0);
+        } else {
+            callbackContext.success(1);
+        }
+
+        return true;
+    }
+
+    protected boolean isMobileQQSupportApi(CallbackContext callbackContext) {
+        boolean isSupport = openApi.isMobileQQSupportApi(OpenConstants.API_NAME_PAY);
+
+        if (!isSupport) {
+            callbackContext.success(0);
+        } else {
+            callbackContext.success(1);
+        }
+
+        return true;
+    }
+
+    protected boolean mqqPay(CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+        // check if # of arguments is correct
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+        } catch (JSONException e) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        PayApi api = new PayApi();
+
+        api.serialNumber = "" + paySerial++;
+        api.callbackScheme = callbackScheme;
+
+        api.pubAcc = ""; // 手Q公众帐号，暂时未对外开放申请
+        api.pubAccHint = ""; // 关注手Q公众帐号提示语
+
+        if (params.has("appId")) {
+            api.appId = params.getString("appId"); // 应用唯一id
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (params.has("nonce")) {
+            api.nonce = params.getString("nonce"); // 随机串
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (params.has("timeStamp")) {
+            api.timeStamp = params.getInt("timeStamp"); // 时间戳
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (params.has("tokenId")) {
+            api.tokenId = params.getString("tokenId"); // QQ钱包的预支付会话标识
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (params.has("bargainorId")) {
+            api.bargainorId = params.getString("bargainorId"); // QQ钱包支付商户号
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (params.has("sigType")) {
+            api.sigType = params.getString("sigType"); // 加密方式
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (params.has("sig")) {
+            api.sig = params.getString("sig"); // 签名串
+        } else {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        if (!api.checkParams()) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        // 调用QPay
+        if (openApi.execApi(api)) {
+            Log.i(TAG, "Payment request has been sent successfully.");
+
+            // save current callback context
+            currentCallbackContext = callbackContext;
+
+            // send no result and keep callback
+            PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
+        } else {
+            Log.i(TAG, "Payment request has been sent unsuccessfully.");
+
+            // send error
+            callbackContext.error(ERROR_SEND_REQUEST_FAILED);
+        }
+
+        return true;
+    }
+
+}
