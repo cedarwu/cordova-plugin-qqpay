a5ec798f6fa40eb15be8bfa286860332de456cd1
cedarwu <cedarwu@tencent.com>
2023-03-06 12:43:01 +0800

    rm payVersion

--- src/android/QQPay.java
+++ src/android/QQPay.java
@@ -196,11 +196,6 @@ public class QQPay extends CordovaPlugin {
         } else {
             api.callbackScheme = callbackScheme;
         }
-        if (params.has("payVersion")) {
-            api.payVersion = params.getString("payVersion");
-        } else {
-            api.payVersion = "1";
-        }
 
         if (!api.checkParams()) {
             Log.e(TAG, api.toString());
